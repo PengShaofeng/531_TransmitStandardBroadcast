e37caf286525841913d8184f35a16ae44fd6ef22
PengShaofeng <3274642663@qq.com>
2019-06-04 22:02:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/a531_transmitstandardbroadcast/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.dell.a531_transmitstandardbroadcast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.dell.a531_transmitstandardbroadcast.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
